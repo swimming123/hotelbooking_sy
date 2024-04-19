fd8f87f241b61b15be75e00ca444b0673efd8721
swimming <48545779+swimming123@users.noreply.github.com>
2024-04-19 22:48:19 +0900

    Pusher 연결해보기 (실패)
    오류메시지 : Failed to load resource: the server responded with a status of 404 ()

---
+++ syyoonHt/src/kr/co/ictedu/mvc/dto/ResvVO.java
@@ -0,0 +1,128 @@
+package kr.co.ictedu.mvc.dto;
+
+public class ResvVO {
+
+
+	private int num;
+	private String fname;
+	private String lname;
+	private String addr1;
+	private String addr2;
+	private String zip;
+	private String tel;
+	private String email;
+	private String dates;
+	private String atime;
+	private String departure1;
+	private String dtime;
+	private String adult;
+	private String children;
+	private String content;
+	private String card;
+	private String cdate;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDates() {
+		return dates;
+	}
+	public void setDate1(String dates) {
+		this.dates = dates;
+	}
+	public String getAtime() {
+		return atime;
+	}
+	public void setAtime(String atime) {
+		this.atime = atime;
+	}
+	public String getDeparture1() {
+		return departure1;
+	}
+	public void setDeparture1(String departure1) {
+		this.departure1 = departure1;
+	}
+	public String getDtime() {
+		return dtime;
+	}
+	public void setDtime(String dtime) {
+		this.dtime = dtime;
+	}
+	public String getAdult() {
+		return adult;
+	}
+	public void setAdult(String adult) {
+		this.adult = adult;
+	}
+	public String getChildren() {
+		return children;
+	}
+	public void setChildren(String children) {
+		this.children = children;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCard() {
+		return card;
+	}
+	public void setCard(String card) {
+		this.card = card;
+	}
+	public String getCdate() {
+		return cdate;
+	}
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+	
+	
+	
+}
